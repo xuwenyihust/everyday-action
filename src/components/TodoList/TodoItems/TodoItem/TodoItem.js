cee5b3c329fc97e68ec56eec0782a141c55bfbbb
xuwenyihust <1016635786@qq.com>
2020-07-05 19:39:57 -0400

    Modify layout.

--- src/components/TodoList/TodoItems/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItems/TodoItem/TodoItem.js
@@ -6,10 +6,13 @@ const todoItem = (props) => (
         <p 
             className={props.item.done ? 'done' : 'undone'}
             onClick={() => props.contentClicked(props.item.id)}>{props.item.content}</p>
+        <button
+            type="button" 
+            className="editButton"><i className="fa fa-bars"></i></button>
         <button 
             type="button" 
             className="closeButton"
-            onClick={() => props.closeClicked(props.item.id)}>&times;</button>
+            onClick={() => props.closeClicked(props.item.id)}><i className="fa fa-close"></i></button>
     </div>
 );
 
