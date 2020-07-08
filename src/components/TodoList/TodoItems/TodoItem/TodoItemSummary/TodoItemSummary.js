811cd3914b84ba71cc2a41afa4a3894b77c69a0d
xuwenyihust <1016635786@qq.com>
2020-07-07 22:23:54 -0400

    Clean up.

--- src/components/TodoList/TodoItems/TodoItem/TodoItemSummary/TodoItemSummary.js
+++ src/components/TodoList/TodoItems/TodoItem/TodoItemSummary/TodoItemSummary.js
@@ -48,53 +48,6 @@ const todoItemSummary = (props) => {
         </div>
 
     );
-
-    
-    // render () {
-    //     const item = this.props.item;
-
-    //     const itemTypes = this.props.itemTypes;
-    //     const itemTypeOptions = itemTypes.map(t => {
-    //         if (item.type === t) {
-    //             return (
-    //                 <option key={t} value={t} selected="selected">{t}</option>
-    //             )
-    //         } else {
-    //             return (
-    //                 <option key={t} value={t}>{t}</option>
-    //             )
-    //         }
-    //     });
-
-    //     return (
-    //         <div className='TodoItemSummary'>
-
-    //             <form>
-    //                 <label>内容：</label>
-    //                 <input 
-    //                     value={this.props.item.content}
-    //                     onChange={this.props.itemSummaryContentChanged}></input>
-    //             </form>
-
-    //             <form>
-    //                 <label>类别：</label>
-    //                 <select
-    //                     onChange={this.props.itemSummaryTypeChanged}>
-    //                     {itemTypeOptions}
-    //                 </select>
-    //             </form>
-
-    //             <button
-    //                 type="button"
-    //                 className="saveButton"
-    //                 onClick={() => this.props.saveClicked(this.props.item)}>确定</button>
-    //             <button
-    //                 type="button" 
-    //                 className="cancelButton"
-    //                 onClick={() => this.props.cancelClicked()}>取消</button>
-    //         </div>
-    //     )
-    // }
 };
 
 export default todoItemSummary;
